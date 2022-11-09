5f1eb73483f339ebf3b6c986724d3a5c5beb53a3
Tanmay <pateltanmayp@gmail.com>
2022-11-09 09:17:20 -0500

    Add message wand and reaction tester projects

---
+++ MsgWandProgram.c
@@ -0,0 +1,101 @@
+/*
+ * File: newmain.c
+ * Author: Tanmay Patel
+ * Project: Message Wand
+ * Description: Displays a message when the wand is waved back and forth (using interrupts).
+ */
+
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/OSC2/CLKOUT pin, I/O function on RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select bit (MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+
+
+#include <xc.h>                 // Import the necessary library
+#define _XTAL_FREQ 4000000      // Set the clock frequency (4MHz)
+
+char totalFrames = 30;          // Total frames that need to be displayed
+char currentFrame = 0;          // Represents which frame is being displayed currently
+char msDelayTime = 4;           // Time between successive frames
+bit dir = 0;                    // 1 is the forward direction, 0 is the reverse direction
+
+// Array stores the values (frame-by-frame) that need to be outputted (each row is a letter)
+char PORTCValues[] = {0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0,    // Space
+                      0b11111111, 0b10010001, 0b10010001, 0b10010001, 0b01101110, 0,    // B
+                      0b11111111, 0b10001000, 0b10001100, 0b10001010, 0b01110001, 0,    // R
+                      0b11111110, 0b00000001, 0b00000001, 0b00000001, 0b11111110, 0,    // U
+                      0b11111111, 0b00001000, 0b00001000, 0b00001000, 0b11111111, 0};   // H
+
+// Initialize all special function registers
+void initialize(void) {
+    ANSEL = 0;  		// Disable analog ports
+    ANSELH = 0;
+    OPTION_REGbits.nRABPU = 0;  // Enable weak pull-ups
+    WPUB = 0b11111111;          // Enable pull-ups specifically in PORTB
+    TRISC = 0b00000000;         // Configure TRISC to output
+    TRISB = 0b11111111;         // Configure TRISB to input
+    PORTC = 0b00000000;         // Clear both ports temporarily (clear any noise)
+    PORTB = 0b00000000;
+    
+    INTCONbits.RABIE = 1;       // Enable change interrupts within PORTA/B
+    INTCONbits.GIE = 1;		// Enable all unmasked interrupts
+    INTCONbits.RABIF = 0;       // Clear the interrupt flag
+    IOCBbits.IOCB5 = 1;         // Instruct the PIC to detect interrupts on change from PORTB bit 5
+    return;
+}
+
+// Display the current frame
+void displayFrame(int frameNum) {
+    PORTC = PORTCValues[frameNum];  // Set PORTC to the current frame's LED pattern
+    return;
+}
+
+// Create a delay of the specified amount (in ms)
+void delay(int msDelay) {
+    
+    // Delay by 1 ms for each iteration
+    for (int i = 0; i < msDelay; i++) {
+        __delay_ms(1);
+    }
+    return;
+}
+
+// Interrupt service routine (called when there is a change on the RB5 line)
+void __interrupt() isr(void) {
+    dir = 1 - PORTBbits.RB5;    // Invert the direction bit
+    INTCONbits.RABIF = 0;       // Clear the interrupt flag
+    return;
+}
+
+// Main logic of the program
+void main(void) {
+    
+    // Main loop
+    while (1) {
+        
+        // If forward direction
+        if (dir == 1) {
+            displayFrame(currentFrame);   // Display the current frame  
+            delay(msDelayTime);           // Delay 4 ms
+            
+            // Reset the current frame if it reaches the max
+            if (currentFrame == totalFrames - 1) {
+                currentFrame = -1;
+            }
+            currentFrame++;               // Increment the current frame          
+        }
+        
+        // If reverse direction
+        else if (dir == 0) {
+            currentFrame = 0;       	  // Reset the current frame back to 0
+            PORTC = 0b00000000;     	  // Keep all LEDs off
+        }
+    }
+    return;
+}
---
+++ ReactionTester.c
@@ -0,0 +1,116 @@
+/*
+ * Author: Tanmay Patel
+ * Description: Reaction Tester Game!
+ *		Press a start button to start the game. A sequence of LEDs will light up after 
+ *		a random delay time. Press the stop button and observe the number of lit LEDs.
+ *		The program caters itself to the user's reaction time, slowing down or speeding
+ *		up when needed.
+ */
+
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/OSC2/CLKOUT pin, I/O function on RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select bit (MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+
+// Import necessary libraries (math.h is needed to generate the random number)
+#include <xc.h>
+#include <math.h>
+#define _XTAL_FREQ 1000000  // Define the crystal frequency
+
+// Integer array stores the state of each LED (0 is off, 1 is on)
+int LEDArray[] = {0, 0, 0, 0, 0, 0, 0, 0};
+int numLEDs = 8; // Number of LEDs used in the program
+
+// Initialize all necessary special function registers 
+void initialize() {
+    // Clear the analog select registers
+    ANSEL = 0;
+    ANSELH = 0;
+    
+    // Enable weak pull-ups for all pins in the PORTB register
+    OPTION_REGbits.nRABPU = 0;
+    WPUB = 0b11111111;
+    
+    // Set PORTC to output and PORTB to input
+    TRISC = 0b00000000;
+    TRISB = 0b11111111;
+    
+    // Initially set both PORTs to off (i.e. clear any "noise")
+    PORTC = 0b00000000;
+    PORTB = 0b00000000;
+}
+
+// Create a specified time delay (in milliseconds)
+void delay(int msDelay) {
+    
+    // Loop the specified number of times
+    for (int i = 0; i < msDelay; i++) {
+        __delay_ms(1);  // Each iteration of the loop delays the program by 1 millisecond
+    }
+}
+
+// Set the status of a specific LED to the specified value (0 is off, 1 is on)
+void setLED(int LEDNum, int value) {
+    LEDArray[LEDNum] = value;
+}
+
+// Convert the LED status values to actual values in PORTC
+void lightLEDs() {
+    PORTCbits.RC0 = LEDArray[0];
+    PORTCbits.RC1 = LEDArray[1];
+    PORTCbits.RC2 = LEDArray[2];
+    PORTCbits.RC3 = LEDArray[3];
+    PORTCbits.RC4 = LEDArray[4];
+    PORTCbits.RC5 = LEDArray[5];
+    PORTCbits.RC6 = LEDArray[6];
+    PORTCbits.RC7 = LEDArray[7];
+}
+
+// Run the primary logic of the program
+void main(void) {
+    initialize(); 	    // Initialize all necessary SFRs
+    
+    int previousScore;      // Represents the number of LEDs lit up in the previous round (governs delay time)
+    int delayTime = 0;      // Time delay between successive LEDs (dependent on previous score)
+    int randomNumSeed = 0;  // Seed used to generate the random number
+    int randomNum = 0;      // The random number used for the initial time delay
+    
+    while(1) {
+        randomNumSeed++; // The seed is incremented in the background when no other function is being performed
+        
+        // Start the LED sequence if the start button (connected to RB5) is pressed
+        if (PORTBbits.RB5 == 0) {
+            randomNum = (int) (5000 * sin(randomNumSeed) + 5000); // Convert the seed to a random number
+            delay(randomNum);                // Create a random time delay (between 0 and 10 seconds)
+            delayTime = previousScore * 60;  // Modify the delay time between successive LEDs as per previous performances
+            
+            // Light successive LEDs
+            for (int i = 0; i < numLEDs; i++) {
+                delay(delayTime);  // Create a short time delay between LEDs
+                setLED(i, 1);      // Set the status of the current LED to on
+                lightLEDs();       // Display the updated LED pattern
+                
+                // Stop the LED sequence if the stop button (connected to RB4) is pressed
+                if (PORTBbits.RB4 == 0) {
+                    previousScore = i; // Update the previous score to the current score
+                    break;
+                }
+            }
+            delay(5000); // Keep the current score frozen for 5 seconds
+            
+            // Reset all LEDs back to off
+            for (int i = 0; i < numLEDs; i++) {
+                setLED(i, 0);
+            }
+            lightLEDs();
+        }
+    }
+    
+    return;
+}
